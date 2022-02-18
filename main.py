2b075236487e1eecc1c45544512c9a596a7cbe90
root <root@DESKTOP-JGB6SLE.localdomain>
2022-02-18 23:51:35 +0100

    v1 main

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-import argparse 
+import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-v","--verbosity", help="hey verbo", action="store_true")
-args = parser.parse_args()
-if args.verbosity:
-	print("verbo one")
+def get_parser(h):
+	parser = argparse.ArgumentParser(add_help=h)
+#	group = parser.add_mutually_exclusive_group()
+#	group.add_argument("-v", "--verbose", action="store_true")
+#	group.add_argument("-q", "--quiet", action="store_true")
+	parser.add_argument("x", help="the base")
+	parser.add_argument("y", help="the exponent")
+#	parser.add_argument("-v", help="the exponent")
+	return parser
 
-
-
+if (__name__=="__main__"):
+	p = get_parser(h=True)
+	args = p.parse_args()
